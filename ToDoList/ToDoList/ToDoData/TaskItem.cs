b8f09b4ada352f10bc06c72b8f107f6aadffbf9a
RemiMuresan <remi.muresan@gmail.com>
2016-04-11 09:55:14 +0300

    add files

---
+++ ToDoList/ToDoList/ToDoData/TaskItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace ToDoData
+{
+    [Table("TaskItems")]
+    public class TaskItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [ForeignKey(typeof(Task))]
+        public int TaskId { get; set; }
+        public string Text { get; set; }
+        public DateTime? ChangedAt { get; set; }
+        public bool IsDone { get; set; }
+        
+    }
+}
